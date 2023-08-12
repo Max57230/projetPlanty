6fd6c682e7e9a0f560d29d0d7b940923b533f99a
Maxime Petit <max57777@hotmail.fr>
2023-08-12 10:15:59 +0200

    ajout des fichiers + début css

---
+++ footer.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package OceanWP WordPress theme
+ */
+
+?>
+
+	</main><!-- #main -->
+		<footer id="site-footer" >
+				<a src="http://planty.local/"> Mentions légales
+		</footer>
+
+		<?php wp_footer(); ?>
+		</body>
+</html>
--- functions.php
+++ functions.php
@@ -6,3 +6,15 @@ function theme_enqueue_styles()
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
 }
+
+function add_admin_link_to_menu($items, $args) {
+    if (is_user_logged_in()) {
+            $menu_items = explode('</li>', $items);
+            $new_item = '<li><a href="' . admin_url() . '">Admin</a></li>';
+            array_splice($menu_items, floor(count($menu_items) / 2), 0 , $new_item);
+            $items = implode('</li>', $menu_items);
+    }
+    return $items;
+}
+
+add_filter('wp_nav_menu_items', 'add_admin_link_to_menu', 10 , 2 );
